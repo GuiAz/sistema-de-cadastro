9e4e6d3dabb678c52a3fa457bc7e038255113cd8
Guilherme Cottas Azevedo <guilhermeazevedo@brq.com>
2023-06-29 20:01:14 -0300

    voltando para o estado do paulo

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogo")
 console.log("aplicação está se conectando ao bando de dados")
-//mais um console log de teste:
 console.log("O Paulo fez esse commit sem voce ver na aula!")
-console.log("Enviando para o GitHub")
