da20f01bb6d28f5a43e844ffacd539087232a302
Guilherme Cottas Azevedo <guilhermeazevedo@brq.com>
2023-06-29 19:44:44 -0300

    console log indicando modificações do paulo

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogo")
 console.log("aplicação está se conectando ao bando de dados")
+console.log("O Paulo fez esse commit sem voce ver na aula!")
