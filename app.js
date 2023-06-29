e9d21e810d8dca134ea1b05e8901df5f419549df
Guilherme Cottas Azevedo <guilhermeazevedo@brq.com>
2023-06-29 19:49:07 -0300

    adicionando um console.log

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogo")
 console.log("aplicação está se conectando ao bando de dados")
+//mais um console log de teste:
 console.log("O Paulo fez esse commit sem voce ver na aula!")
+console.log("Enviando para o GitHub")
